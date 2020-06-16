1c1be5f95408ab8e01ba5cbf16201ddf4ec673aa
mattbesedick <mattbezy@gmail.com>
2020-06-15 19:09:47 -0500

    updated solution with prompt/comments

--- rot13.js
+++ rot13.js
@@ -1,5 +1,8 @@
-function rot13(message) {
+/*ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. ROT13 is an example of the Caesar cipher.
+Create a function that takes a string and returns the string ciphered with Rot13. If there are numbers or special characters included in the string, 
+they should be returned as they are. Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation". */
 
+function rot13(message) {
     //probably can get a solution without two maps here, just what I thought of right away.
     const lowercaseLetters = [['a', 0], ['b', 1], ['c', 2], ['d', 3], ['e', 4],
     ['f', 5], ['g', 6], ['h', 7], ['i', 8],
@@ -24,7 +27,9 @@ function rot13(message) {
     let find, cipherChar, findKey;
     let answer = []
     for (let i in messageArray) {
-        if (messageArray[i] === messageArray[i].toUpperCase()) {
+
+        //probably can use helper functions for some of these calculations/since they're repeating 
+        if (messagArray[i] === messageArray[i].toUpperCase()) {
             find = uppercaseMap.get(messageArray[i])
             find = find + 13
             if (find > 25) {
